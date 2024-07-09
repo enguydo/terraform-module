1438539a614519782208f4f07bfb707c19602fbf
enguydo <donguyenquocan@gmail.com>
2024-07-09 10:50:38 -0500

    change vpc= true to domain="vpc"

--- nat-gateway/main.tf
+++ nat-gateway/main.tf
@@ -1,6 +1,6 @@
 # allocate elastic ip. this eip will be used for the nat-gateway in the public subnet az1 
 resource "aws_eip" "eip1" {
-  vpc = true
+  domain = "vpc"
 
   tags = {
     Name = "${var.project_name}-${var.environment}-eip1"
@@ -9,7 +9,7 @@ resource "aws_eip" "eip1" {
 
 # allocate elastic ip. this eip will be used for the nat-gateway in the public subnet az2
 resource "aws_eip" "eip2" {
-  vpc = true
+  domain = "vpc"
 
   tags = {
     Name = "${var.project_name}-${var.environment}-eip2"
@@ -93,5 +93,5 @@ resource "aws_route_table_association" "private_app_subnet_az2_rt_az2_associatio
 # associate private data subnet az2 with private route table az2
 resource "aws_route_table_association" "private_data_subnet_az2_rt_az2_association" {
   subnet_id      = var.private_data_subnet_az2_id
-  route_table_id = ws_route_table.private_route_table_az2.id
+  route_table_id = aws_route_table.private_route_table_az2.id
 }
