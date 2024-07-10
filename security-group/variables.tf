9bec2f6f6b066f49007427e3f248146d3535e3fa
enguydo <donguyenquocan@gmail.com>
2024-07-10 10:56:28 -0500

    security group

---
+++ security-group/variables.tf
@@ -0,0 +1,7 @@
+# security groups variables
+variable "project_name" {}
+variable "environment" {}
+variable "vpc_id" {}
+variable "ssh_ip" {}
+variable "project_name" {}
+variable "project_name" {}
